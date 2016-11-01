d68bf47afcb665ed1d5d2672fe6be7867f09fa80
Andreev Boghdan <boghdanandreev@gmail.com>
2016-11-01 11:21:26 +0200

    stucture added

--- questions/urls.py
+++ questions/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import url
-from django.contrib import admin
 from questions import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', views.manage, name='manage'),
+    url(r'^questions/(?P<question_number>\d+)/$', views.questions, name='questions'),
+
 ]
---
+++ questions/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def manage(request):
+	pass
+
+def questions(request, question_number=None):
+	pass
